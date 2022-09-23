2d3a6366ede78f9470efe7e856f1c648e27d46dc
Dmitrii <elfin85@gmail.com>
2022-09-23 01:27:47 +0100

    Solving eleven problems in JS

---
+++ main.js
@@ -0,0 +1,57 @@
+// JS Freecodecamp
+// Первое задание
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+// Второе задание
+var myName;
+
+//Третье задание
+var a;
+a = 7;
+
+//Четвертое задание
+var a;
+a = 7;
+var b;
+b = a;
+
+//Пятое задание
+var a = 9;
+
+//Шестое задание
+var myFirstName = "Dmitrii";
+var myLastName = "Iakovenko"
+
+//Седьмое задание
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Восьмое задание
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Девятое задание,
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Десятое задание
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+//Одиннадцатое задание
+const sum = 10 + 10;
